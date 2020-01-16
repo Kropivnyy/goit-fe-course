46d6679c1449abd3210cacd9be2ee83e9b3b44f2
Alexander Kropivnyy <kropivnyy@gmail.com>
2020-01-16 18:45:35 +0200

    added prettierrc

--- javascript/module-02/js/task-01.js
+++ javascript/module-02/js/task-01.js
@@ -1,11 +1,10 @@
-// 'use strict';
-// const logItems = function(array) {
-//   for (let i = 0; i < array.length; i++) {
-//     const element = array[i];
-//     console.log(`${i+1} - ${element}`);
-//   }
-// };
+const logItems = function(array) {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(`${i+1} - ${element}`);
+  }
+};
 
-// logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
-// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
--- javascript/module-02/js/task-04.js
+++ javascript/module-02/js/task-04.js
@@ -1,26 +1,26 @@
-// const formatString = function(string) {
-//   if (string.length <= 40) {
-//     return string;
-//   } else {
-//     string = string.split('');
-//     string.length = 40;
-//     string = string.join('');
-//     return string + '...';
-//   }
-// };
+const formatString = function(string) {
+  if (string.length <= 40) {
+    return string;
+  } else {
+    string = string.split('');
+    string.length = 40;
+    string = string.join('');
+    return string + '...';
+  }
+};
 
-// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
-// // вернется оригинальная строка
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
 
-// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
-// // вернется форматированная строка
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
 
-// console.log(formatString('Curabitur ligula sapien.'));
-// // вернется оригинальная строка
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
 
-// console.log(
-//   formatString(
-//     'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
-//   ),
-// );
-// // вернется форматированная строка
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
+// вернется форматированная строка
