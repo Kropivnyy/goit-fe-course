f7ba5b81301329ab22401a03454bd9d2063db76e
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2020-02-20 07:19:29 +0200

    added sixth homework

--- javascript/module-06/js/module-06.js
+++ javascript/module-06/js/module-06.js
@@ -13,8 +13,7 @@ console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley
 
 console.log(`Task 03`);
 const getUsersWithGender = (users, gender) => {
-  const filterArray = () => users.filter(user => user.gender === gender);
-  return filterArray().map(user => user.name);
+  return users.filter(user => user.gender === gender).map(user => user.name);
 };
 
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
@@ -41,6 +40,42 @@ console.log(getUsersWithAge(users, 30, 40));
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 console.log(`Task 07`);
+const calculateTotalBalance = users =>
+  users.reduce((acc, user) => acc + user.balance, 0);
+
+console.log(calculateTotalBalance(users)); // 20916
+
 console.log(`Task 08`);
+const getUsersWithFriend = (users, friendName) => {
+  return users
+    .filter(user => user.friends.includes(friendName))
+    .map(user => user.name);
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+
 console.log(`Task 09`);
+const getNamesSortedByFriendsCount = users => {
+  const sortByFriendsCount = (a, b) => a.friends.length - b.friends.length;
+  return users.sort(sortByFriendsCount).map(user => user.name);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
 console.log(`Task 10`);
+const getSortedUniqueSkills = users => {
+  return users
+    .reduce((allSkills, user) => {
+      allSkills.push(...user.skills);
+      return allSkills;
+    }, [])
+    .sort()
+    .filter((skill, index, array) => {
+      return array.indexOf(skill) === index;
+    });
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
